b87612799a5343c19c60859e1f54192627791b94
Krystof Rehacek <krystofee@gmail.com>
2021-05-25 12:51:41 +0200

    Created messenger app

--- App.tsx
+++ App.tsx
@@ -2,20 +2,15 @@ import React, { useEffect, useState } from "react";
 import {
   Spinner,
   Container,
-  Button,
-  Icon,
   Header,
-  Left,
   Body,
-  Right,
-  Content,
-  Footer,
-  FooterTab,
   Title,
+  Left,
+  Right,
 } from "native-base";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Text } from "react-native";
+import { Platform, KeyboardAvoidingView } from "react-native";
 import ChatClient from "./ChatClient";
 
 const App = () => {
@@ -39,33 +34,18 @@ const App = () => {
   return (
     <Container>
       <Header>
-        <Left>
-          <Button transparent>
-            <Icon name="menu" />
-          </Button>
-        </Left>
+        <Left></Left>
         <Body>
-          <Title>Chat app</Title>
+          <Title>Messenger</Title>
         </Body>
-        <Right>
-          <Button transparent>
-            <Icon type="FontAwesome" name="plus" />
-          </Button>
-        </Right>
+        <Right></Right>
       </Header>
-      <Content>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
         <ChatClient />
-      </Content>
-      {/* <Button>
-            <Text>Test button</Text>
-          </Button> */}
-      <Footer>
-        <FooterTab>
-          <Button full>
-            <Text>Footer</Text>
-          </Button>
-        </FooterTab>
-      </Footer>
+      </KeyboardAvoidingView>
     </Container>
   );
 };
--- ChatClient.tsx
+++ ChatClient.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
-import { View } from "react-native";
-import { Text, Button, Card } from "native-base";
+import { Keyboard, TouchableWithoutFeedback, View } from "react-native";
+import { Text, Button, Input, Form, Icon } from "native-base";
 import io from "socket.io-client";
 
 const ChatClient = () => {
@@ -17,31 +17,72 @@ const ChatClient = () => {
       setMessages((old) => [...old, msg]);
     });
     socketRef.current = newSocket;
+
+    return () => {
+      newSocket.disconnect();
+    };
   }, []);
 
   const handleSend = () => {
-    socket.emit("chat message", "Test message!");
-  };
-
-  const handleClear = () => {
-    setMessages([]);
+    socket.emit("chat message", message);
+    setMessage("");
   };
 
   return (
-    <View>
-      {messages.map((item) => (
-        <Card>
-          <Text>{item}</Text>
-        </Card>
-      ))}
+    <View style={{ flex: 1 }}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={{ flex: 1 }}>
+        <View
+          style={{ flexGrow: 1, paddingHorizontal: 16, paddingVertical: 8 }}
+        >
+          {messages.length === 0 && (
+            <View
+              style={{
+                alignItems: "center",
+                padding: 8,
+                marginVertical: 16,
+                backgroundColor: "#ffffff",
+              }}
+            >
+              <Text style={{ color: "#999" }}>No messages yet...</Text>
+            </View>
+          )}
+          {messages.map((item) => (
+            <View
+              style={{
+                padding: 8,
+                marginVertical: 8,
+                borderStyle: "solid",
+                borderColor: "#efefef",
+                borderWidth: 1,
+                borderRadius: 6,
+                backgroundColor: "#fafafa",
+              }}
+            >
+              <Text style={{ color: "#333" }}>{item}</Text>
+            </View>
+          ))}
+        </View>
+      </TouchableWithoutFeedback>
 
-      <Button onPress={handleSend}>
-        <Text>Send</Text>
-      </Button>
-
-      <Button onPress={handleClear}>
-        <Text>Clear</Text>
-      </Button>
+      <Form
+        style={{
+          paddingHorizontal: 4,
+          borderStyle: "solid",
+          borderColor: "#efefef",
+          borderWidth: 1,
+        }}
+      >
+        <View style={{ flexDirection: "row" }}>
+          <Input
+            placeholder="Type message here"
+            value={message}
+            onChangeText={(value) => setMessage(value)}
+          />
+          <Button transparent onPress={handleSend}>
+            <Icon name="send" />
+          </Button>
+        </View>
+      </Form>
     </View>
   );
 };
